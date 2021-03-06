5043ddffc22c43199250b864d9baf402ba5feb5b
‘name’ <email>
2021-03-07 06:19:13 +0800

    这个是commit进行提交

--- src/test/java/OpenBaidu.java
+++ src/test/java/OpenBaidu.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class OpenBaidu {
@@ -8,6 +9,7 @@ public class OpenBaidu {
         WebDriver webDriver=null;
         System.setProperty("webdriver.chrome.driver", ".\\driverBrower\\chromedriver.exe");
         webDriver = new ChromeDriver();
+        Assert.assertEquals(1,2);
         webDriver.get("https://www.baidu.com/");
         Thread.sleep(5000);
         webDriver.quit();
